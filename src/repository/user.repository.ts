afacd3c19109c37b0680c1f306662f09c28bfdde
Vitor Batista <42703631+vitorsantosb@users.noreply.github.com>
2024-10-22 00:54:43 -0300

    Update: Update user repository

--- src/repository/user.repository.ts
+++ src/repository/user.repository.ts
@@ -1,63 +1,100 @@
-import {IUser} from "./dto/user.interface";
-import {prisma} from '@database/prisma.database';
+import { IUser } from './dto/user.interface';
+import { prisma } from '@database/prisma.database';
 
 
 async function StoreUser(user: IUser) {
-	return prisma.user.create(
-		{
-			data: {
-				id: user.id,
-				name: user.name,
-				email: user.email,
-				password: user.password,
-				chat_rooms: {connect: []}
-			}
-		}
-	)
+  return prisma.user.create(
+    {
+      data: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        password: user.password,
+        chat_rooms: { connect: [] },
+      },
+    },
+  );
 }
 
 async function GetUser(_id: string) {
-	return prisma.user.findUnique({
-		where: {
-			id: _id
-		}
-	});
+  return prisma.user.findUnique({
+    where: {
+      id: _id,
+    },
+  });
 }
 
 async function GetUserDataWithEmail(_email: string) {
-	return prisma.user.findUnique({
-		where: {
-			email: _email
-		}
-	})
+  return prisma.user.findUnique({
+    where: {
+      email: _email,
+    },
+  });
 }
 
 async function ExistsUserWithEmail(_email: string) {
-	return prisma.user.findUnique({
-		where: {
-			email: _email
-		}
-	})
+  return prisma.user.findUnique({
+    where: {
+      email: _email,
+    },
+  });
 }
 
 async function ExistsAndGetUserWithId(_id: string) {
-	return prisma.user.findUnique({
-		where: {
-			id: _id
-		}
-	})
+  return prisma.user.findUnique({
+    where: {
+      id: _id,
+    },
+  });
 }
 
-async function RegisterUserSession(_id: string, _sessionData: any) {
+async function GetUsersListWithPagination(_page: number, _limit: number) {
+  return prisma.user.findMany(
+    {
+      skip: _page,
+      take: _limit,
+    },
+  );
+}
 
+async function GetUsersListWithPaginationBasedOnUsername(_page: number, _limit: number, _name: string) {
+  return prisma.user.findMany(
+    {
+      skip: _page,
+      take: _limit,
+      where: {
+        name: _name,
+      },
+    },
+  );
+}
+
+async function GetUsersList(){
+  return prisma.user.findMany();
+}
+
+async function CountUsersWithName(_name: string) {
+  return prisma.user.count({
+    where: {
+      name: _name,
+    },
+  });
+}
+
+async function CountTotalUsers(){
+  return prisma.user.count();
 }
 
 export {
-	RegisterUserSession,
-	StoreUser,
-	ExistsUserWithEmail,
-	GetUser,
-	GetUserDataWithEmail,
-	ExistsAndGetUserWithId
-}
+  StoreUser,
+  ExistsUserWithEmail,
+  GetUser,
+  GetUserDataWithEmail,
+  ExistsAndGetUserWithId,
+  GetUsersListWithPagination,
+  GetUsersListWithPaginationBasedOnUsername,
+  GetUsersList,
+  CountTotalUsers,
+  CountUsersWithName,
+};
 
